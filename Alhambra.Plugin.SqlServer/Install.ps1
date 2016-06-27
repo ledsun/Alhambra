c5676103f7107a01d6339f197bb307404c4394d2
ledsun <shigeru.nakajima@gmail.com>
2016-06-27 22:24:20 +0900

    Publish a new nuget package of the Alhambra.Plugin.SqlServer

---
+++ Alhambra.Plugin.SqlServer/Install.ps1
@@ -0,0 +1,3 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$project.ProjectItems.Item("Plugins").ProjectItems.Item("Alhambra.Plugin.SqlServer.dll").Properties.Item("CopyToOutputDirectory").Value = 2
